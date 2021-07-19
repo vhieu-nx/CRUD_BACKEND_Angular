e8545940ec04e138278d248f85c3934ae2b27345
vhieu-nx <nguyenvhieu1208@gmail.com>
2021-07-19 13:06:57 +0700

    crud search api backend

---
+++ src/main/java/com/codegym/crud/repository/TutorialRepository.java
@@ -0,0 +1,11 @@
+package com.codegym.crud.repository;
+
+import com.codegym.crud.model.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+	List<Tutorial> findByPublished(boolean published);
+	List<Tutorial> findByTitleContaining(String title);
+}
